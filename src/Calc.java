d0a95524e0d79c32f8cc26010dfd55919acb9315
BT_3W2_04 <vincentl105@Nycstudents.net>
2022-12-06 09:27:12 -0500

    main class 80% finished

--- src/Calc.java
+++ src/Calc.java
@@ -1,6 +1,7 @@
 public class Calc {
     private double answer = 0;
     private boolean roundToInt;
+    private String equationS;
     public Calc () {
     }
 
@@ -8,30 +9,40 @@ public class Calc {
         this.roundToInt = roundToInt;
     }
 
-    public String equation (String equa) {
-        while(!equa.contains("+") && !(equa.contains("-") && isNumeric(equa.substring(equa.indexOf("-") + 1))) && !equa.contains("*") && !equa.contains("/")) {
-            if (equa.contains("*")) {
-                answer += equationWithIn(equa, "*");
-            }
-            if (equa.contains("/")) {
-                answer += equationWithIn(equa, "/");
-            }
-            if (equa.contains("-") && !hasNegative(equa)) {
-                answer += equationWithin(equa, "-");
-            }
-            if (equa.contains("+")) {
-                answer += equationWithIn(equa, "+");
+    public void simplifyParenthesis (String equation) {
+        while (equation.contains("(") && equation.contains(")")) {
+            String equa = equation.substring(equation.indexOf ("(") + 1, equation.indexOf(")"));
+            while(!equa.contains("+") && !(equa.contains("-") && isNumeric(equa.substring(equa.indexOf("-") + 1))) && !equa.contains("*") && !equa.contains("/")) {
+                if (equa.contains("/")) {
+                    equation = equation.replace("(" + equa + ")",equationWithIn(equa, "/") + "");
+                }
+                if (equa.contains("*")) {
+                    equation = equation.replace("(" + equa + ")",equationWithIn(equa, "*") + "");
+                }
+                if (equa.contains("-") && !hasNegative(equa)) {
+                    equation = equation.replace("(" + equa + ")",equationWithIn(equa, "-") + "");
+                }
+                if (equa.contains("+")) {
+                    equation = equation.replace("(" + equa + ")",equationWithIn(equa, "+") + "");
+                }
             }
         }
+        equationS = equation;
     }
 
     public double equationWithIn (String equation, String sign) {
         boolean equationFinished = false;
         String left = "";
         String right = "";
-        for (int i = 1; equationFinished != false; i ++) {
+        for (int i = 1; equationFinished == false; i ++) {
             String L = equation.substring (equation.indexOf(sign) - i);
+            if (hasNegative(equation.substring(equation.indexOf(L) - 2))) {
+                L = "-" + L;
+            }
             String R = equation.substring(equation.indexOf(sign) + i);
+            if (hasNegative(equation.substring(equation.indexOf(R) - 2))) {
+                R = "-" + R;
+            }
             if (isNumeric(L) && isNumeric(R)) {
                 left = L + left;
                 right += R;
@@ -74,7 +85,7 @@ public class Calc {
 
     public boolean hasNegative (String equation) {
         boolean has = false;
-        for (int i = 0; i < equation.length(); i ++) {
+        for (int i = 0; i < equation.length(); i++) {
             equation = equation.substring(equation.indexOf("-") + 1);
             int index = equation.indexOf("-");
             if (index != -1 && !isNumeric(equation.substring(index - 1, index))) {
@@ -82,5 +93,37 @@ public class Calc {
             }
         }
         return has;
+    }
+
+    public double calculateFinal (String equation) {
+        while(!equation.contains("+") && !(equation.contains("-") && isNumeric(equation.substring(equation.indexOf("-") + 1))) && !equation.contains("*") && !equation.contains("/")) {
+            if (equation.contains("/")) {
+                answer += equationWithIn(equation, "/");
+            }
+            if (equation.contains("*")) {
+                answer += equationWithIn(equation, "*");
+            }
+            if (equation.contains("-") && !hasNegative(equation)) {
+                answer += equationWithIn(equation, "-");
+            }
+            if (equation.contains("+")) {
+                answer += equationWithIn(equation, "+");
+            }
+        }
+        return answer;
+    }
+
+    public String toString() {
+        String toString = "The simplified equation would be: " + equationS;
+        return toString;
+    }
+
+    public static boolean validEquation(String equation) {
+        for (int i = 0; i < equation.length(); i ++) {
+            if (isNumeric(equation.substring(i, i + 1))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
 public class Main {
+    public static void main (String [] args) {
+        String equation = "";
+        String round = "";
+        Scanner input = new Scanner (System.in);
+
+        System.out.println ("Hello!\nWelcome to the simple arithmetic calculator.");
+        System.out.println ("Please type in your equation: ");
+        equation = input.nextLine();
+        while (!Calc.validEquation(equation)) {
+            System.out.println ("\\*\\*Please type in a valid equation!/*/");
+            System.out.println ("Please type in your equation: ");
+            equation = input.nextLine();
+        }
+        System.out.println ("Got it!");
+        System.out.println ("Would you like for me to round your answer to the nearest integer? (y/n)");
+        round = input.nextLine();
+        while (!round.equals ("y") || !round.equals ("n")) {
+            System.out.println ("Please decide if you want to round the answer to the nearest integer! (y/n)");
+            round = input.nextLine();
+        }
+        if (round.equals ("y")) {
+            Calc calculator = new Calc (true);
+        }
+        else {
+            Calc calculator = new Calc (false);
+        }
+    }
 }
